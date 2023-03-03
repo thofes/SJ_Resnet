4540e9d0fccd723c494c3910e082535c53680621
thofes <hofertommy@hotmail.com>
2023-03-03 13:27:08 +0100

    First Commit of new Repository

---
+++ dataloader.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+print("dataloader module imported successfully")
+def load_data(train_dir, val_dir, image_size=(270,480), batch_size=50):
+    # Load the data
+    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        train_dir,
+        subset="training",
+        validation_split = 0.2,
+        seed=123,
+        image_size=image_size,
+        batch_size=batch_size
+    )
+
+    val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        val_dir,
+        subset="validation",
+        validation_split = 0.2,
+        seed=123,
+        image_size=image_size,
+        batch_size=batch_size
+    )
+
+    class_names = train_ds.class_names
+
+    return train_ds, val_ds, class_names
---
+++ learning_func.py
@@ -0,0 +1,25 @@
+import tensorflow_addons as tfa
+
+def Ranger(sync_period=6,
+           slow_step_size=0.5,
+           learning_rate=0.001,
+           beta_1=0.9,
+           beta_2=0.999,
+           epsilon=1e-7,
+           weight_decay=0.,
+           amsgrad=False,
+           sma_threshold=5.0,
+           total_steps=0,
+           warmup_proportion=0.1,
+           min_lr=0.,
+           name="Ranger"):
+    inner = tfa.optimizers.RectifiedAdam(learning_rate, beta_1, beta_2, epsilon, weight_decay, amsgrad, sma_threshold, total_steps, warmup_proportion, min_lr, name)
+    optim = tfa.optimizers.Lookahead(inner, sync_period, slow_step_size, name)
+    return optim
+
+
+def scheduler(epoche, lr):
+      if epoche < 2:
+          return lr
+      else:
+          return lr*0.97
